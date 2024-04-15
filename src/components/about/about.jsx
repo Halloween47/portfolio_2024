a75317bb3c42b2b0970e1944d95b62b507325ff9
Thomas Leconte <thomasl.dw20@gmail.com>
2024-04-15 14:01:52 +0200

    Prototype Texte About

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -18,7 +18,8 @@ function About() {
       </div>
       <div className="about_droite">
         <div className="whoami" id="whoami">
-          Qui suis-je ?
+          
+          <p>Qui suis-je ?<br/>Passionné par la technologie sous toutes ses formes, mon parcours a débuté avec une formation en conception de prototypes numériques, explorant diverses technologies telles que l'impression 3D, la réalité virtuelle, et l'Internet des objets. Cette expérience m'a conduit à découvrir ma passion pour le web design, où j'ai développé des compétences en création d'interfaces utilisateur esthétiques et conviviales. Par la suite, j'ai choisi de me spécialiser dans le développement front-end avec React, attiré par sa flexibilité et sa capacité à créer des interfaces interactives tout en gardant une sensibilité pour l'UX et l'UI.</p>
         </div>
         <div className="certificate" id="certificate">
           Diplome

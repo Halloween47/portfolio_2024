ddfe35743c2785b3332ac0bd6804e40edfc55df7
Thomas Leconte <thomasl.dw20@gmail.com>
2024-04-16 14:58:23 +0200

    renommage class

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -38,6 +38,7 @@ function About() {
           <p>
             Formations professionnelles
             <br />
+            <br />
             <ShowMore nom="nomTesst" />
           </p>
         </div>
--- src/components/showMore/showMore.jsx
+++ src/components/showMore/showMore.jsx
@@ -8,22 +8,33 @@ import Typography from "@mui/material/Typography";
 function ShowMore(props) {
   return (
     <div className="show-more">
-      <p>test {props.nom}</p>
-      <Accordion className="testColor">
+      {/* <p>test {props.nom}</p> */}
+      <Accordion className="accordion">
         <AccordionSummary
           expandIcon={<ArrowDropDownIcon />}
           aria-controls="panel1-content"
           id="panel1-header"
           style={{ backgroundColor: 'lightgreen' }}
         >
-          <Typography>Développeur d'applications JavaScript React</Typography>
-          <Typography sx={{ color: 'text.secondary' }}>Decembre 2022 - Fevrier 2024</Typography>
+          <Typography className="accordionTypography">Développeur d'applications JavaScript React</Typography>
+          <Typography className="accordionTypography" sx={{ color: 'text.secondary' }}> 2022 - 2024</Typography>
         </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <AccordionDetails className="accordionDetails">
+          {/* <Typography>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          </Typography> */}
+          <Typography>RNCP Niveau 6 (bac +3/4) - OpenClassRooms</Typography>
+          <ul class="">
+              <li>Intégration de maquettes (HTML, CSS)</li>
+              <li>Responsive web design</li>
+              <li>Animation CSS3</li>
+              <li>JavaScript (DOM, Event, ...)</li>
+              <li>Accessibilité</li>
+              <li>Création d'application ReactJS</li>
+              <li>Système de gestion d'état avec Redux</li>
+              <li>Optimisation des performances</li>
+            </ul>
         </AccordionDetails>
       </Accordion>
     </div>

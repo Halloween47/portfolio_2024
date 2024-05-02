75167204af94ffee82429a1115481c8be350ae39
Thomas Leconte <thomasl.dw20@gmail.com>
2024-05-02 21:54:34 +0200

    Installation fontawesome OK

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,23 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 function Menu() {
   return (
     <div className="menu">
       <div id="hamburger">
-        <span></span>
-        <span></span>
-        <span></span>
+        <FontAwesomeIcon icon={faBars} />
       </div>
       <h1>Menu</h1>
-      {/* <div id="menu-mobile">
-        <div id="bg-menu-mobile"></div>
-
-        <ul>
-          <li>Empresa</li>
-          <li>Notícias</li>
-          <li>Agenda</li>
-          <li>Comunicação</li>
-          <li>Contactos</li>
-        </ul>
-      </div> */}
     </div>
   );
 }

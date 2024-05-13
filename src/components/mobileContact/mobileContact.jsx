5a6cc91fe532e950e6a13d7983be2b71c665229b
Thomas Leconte <thomasl.dw20@gmail.com>
2024-05-13 22:14:35 +0200

    MAJ Prototype Mobile Contact

--- src/components/mobileContact/mobileContact.jsx
+++ src/components/mobileContact/mobileContact.jsx
@@ -3,7 +3,9 @@ import FolderList from "./folderList"
 function MobileContact() {
     return(
         <div className="mobile-contact">
+            <h2>Contact</h2>
 <FolderList />
+<button>CV</button>
         </div>
     )
 }

4ef6bbb16094e013addea2a9133745ed0fa8a9ff
Thomas Leconte <thomasl.dw20@gmail.com>
2024-05-16 16:17:54 +0200

    Prototype Mobile About ME !

--- src/components/mobileAbout/mobileAbout.jsx
+++ src/components/mobileAbout/mobileAbout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import MobileAboutMe from '../mobileAboutMe/mobileAboutMe'
 
 const MobileAbout = () => {
   return (
     <div className='MobileAbout'>
         <div className="MobileAboutNav">MobileAboutNav</div>
-        <div className="MobileAboutCompenents">MobileAboutCompenents</div>
+        <div className="MobileAboutCompenents">
+          MobileAboutCompenents
+          <MobileAboutMe />
+          </div>
         </div>
   )
 }
---
+++ src/components/mobileAboutMe/mobileAboutMe.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MobileAboutMe = () => {
+  return (
+    <div className="mobileAboutMe">
+        <p className="nomPrenom">Thomas Leconte</p>
+        <p className="fonction">Développeur Javascript React</p>
+        <div className="texteAboutMe">
+            Qui suis-je ? 
+            <br/> 
+            <br/> 
+            Passionné par la technologie sous toutes ses formes, mon parcours a débuté avec une formation en conception de prototypes numériques, explorant diverses technologies telles que l'impression 3D, la réalité virtuelle, et l'Internet des objets. Cette expérience m'a conduit à découvrir ma passion pour le web design, où j'ai développé des compétences en création d'interfaces utilisateur esthétiques et conviviales. Par la suite, j'ai choisi de me spécialiser dans le développement front-end avec React, attiré par sa flexibilité et sa capacité à créer des interfaces interactives tout en gardant une sensibilité pour l'UX et l'UIU.
+            <br/> 
+            <br/> 
+            <br/> 
+        </div>
+    </div>
+  )
+}
+
+export default MobileAboutMe
